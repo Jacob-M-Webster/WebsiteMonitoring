be69c5082a4c8f745a779ed73b4d67e9271f58d7
Jacob-M-Webster <jakewebster5619@gmail.com>
2026-03-23 10:43:47 -0400

    Significant progress on the logic behind pinging and checking if websites need to be checked. TODO: finish the connection between workers and DB. Currently no found info is saved to the Checks table.

--- monitoring-worker/src/index.ts
+++ monitoring-worker/src/index.ts
@@ -1,9 +1,50 @@
-import { Hono } from 'hono'
+import { createClient } from '@supabase/supabase-js'
 
-const app = new Hono()
+interface Env {
+  SUPABASE_URL: string
+  SUPABASE_ANON_KEY: string
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+async function pingURL(url: string) {
+  try {
+    const start_time = Date.now()
+    const response = await fetch(url)
+    const latency = start_time - Date.now()
+    const status_code = response.status
 
-export default app
+    if (status_code >= 200 && status_code < 300) {
+      if (latency >= 150) {
+        return {status: "up with high latency", latency: latency}
+      } else {
+        return {status: "up", latency: latency}
+      }
+
+    } else if (status_code >= 400 && status_code < 600){
+      return {status: "down", latency: latency}
+    }
+  } catch (error) {
+    return { status: 'down', latency: 0 }
+  }
+}
+
+async function runChecks(supabase: any) {
+  const { data, error } = await supabase.rpc('check_intervals')
+  
+  if (error || !data) {
+    return
+  }
+
+  for (const monitor of data) {
+    const result = pingURL(monitor.url)
+
+    
+
+  }
+}
+
+export default {
+  async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
+    const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
+    ctx.waitUntil(runChecks(supabase))
+  }
+}
